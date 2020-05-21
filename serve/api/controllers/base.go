44c5063496871f82a0bd4a93ca3ba00c9e285943
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-21 15:41:53 +0700

    first api

---
+++ serve/api/controllers/base.go
@@ -0,0 +1,58 @@
+package controllers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres" //postgres
+
+	"serve/api/middlewares"
+	"serve/api/models"
+	"serve/api/responses"
+)
+
+// app
+type App struct {
+	Router *mux.Router
+	DB     *gorm.DB
+}
+
+// Initialize connect to the database and wire up routes
+func (a *App) Initialize(DbHost, DbPort, DbUser, DbName, DbPassword string) {
+	var err error
+	DBURI := fmt.Sprintf("host=%s port=%s user=%s dbname=%s sslmode=disable password=%s", DbHost, DbPort, DbUser, DbName, DbPassword)
+
+	a.DB, err = gorm.Open("postgres", DBURI)
+	if err != nil {
+		fmt.Printf("\n Cannot connect to database %s", DbName)
+		log.Fatal("This is the error:", err)
+	} else {
+		fmt.Printf("We are connected to the database %s", DbName)
+	}
+
+	a.DB.Debug().AutoMigrate(&models.User{}) //database migration
+
+	a.Router = mux.NewRouter().StrictSlash(true)
+	a.initializeRoutes()
+}
+
+func (a *App) initializeRoutes() {
+	a.Router.Use(middlewares.SetContentTypeMiddleware) // setting content-type to json
+
+	a.Router.HandleFunc("/", home).Methods("GET")
+	a.Router.HandleFunc("/register", a.UserSignUp).Methods("POST")
+	a.Router.HandleFunc("/login", a.Login).Methods("POST")
+}
+
+// app run local
+func (a *App) RunServer() {
+	log.Printf("\nServer starting on port 9000")
+	log.Fatal(http.ListenAndServe(":9000", a.Router))
+}
+
+func home(w http.ResponseWriter, r *http.Request) { // this is the home route
+	responses.JSON(w, http.StatusOK, "Welcome To serve")
+}
