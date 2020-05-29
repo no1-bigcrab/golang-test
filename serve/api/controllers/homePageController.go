7f7eeccfe936ee8f8ae328c1a1341aef03cd8a26
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-29 17:38:07 +0700

    update form post

--- serve/api/controllers/homePageController.go
+++ serve/api/controllers/homePageController.go
@@ -59,9 +59,10 @@ func (a *App) HomePageGet(w http.ResponseWriter, r *http.Request) {
 func (a *App) HomePagePost(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	url := "https://" + r.FormValue("apikey") + ":" + r.FormValue("password") + "@" + r.FormValue("hostname") + "/admin/api/2020-04/" + r.FormValue("api-title") + ".json"
+	title := r.FormValue("api-title")
 
-	if r.FormValue("api-title") == "products" {
-
+	switch title {
+	case "products":
 		values := map[string]map[string]string{
 			"product": {
 				"title":       r.FormValue("title"),
@@ -70,46 +71,9 @@ func (a *App) HomePagePost(w http.ResponseWriter, r *http.Request) {
 				"productType": r.FormValue("product-type"),
 			},
 		}
-		jsonValue, _ := json.Marshal(values)
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
-		req.Header.Set("Content-Type", "application/json")
+		checkValueRequest(values, url)
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("response Status:", resp.Status)
-		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
-
-	} else if r.FormValue("api-title") == "blogs" {
-
-		values := map[string]map[string]string{
-			"blog": {
-				"title":      r.FormValue("title"),
-				"metafields": "global",
-			},
-		}
-		jsonValue, _ := json.Marshal(values)
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
-		req.Header.Set("Content-Type", "application/json")
-
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("response Status:", resp.Status)
-		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
-
-	} else if r.FormValue("api-title") == "pages" {
-
+	case "pages":
 		values := map[string]map[string]string{
 			"page": {
 				"title":     r.FormValue("title"),
@@ -117,22 +81,17 @@ func (a *App) HomePagePost(w http.ResponseWriter, r *http.Request) {
 				"author":    r.FormValue("author"),
 			},
 		}
-		jsonValue, _ := json.Marshal(values)
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
-		req.Header.Set("Content-Type", "application/json")
+		checkValueRequest(values, url)
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
+	case "blogs":
+		values := map[string]map[string]string{
+			"blog": {
+				"title": r.FormValue("title"),
+			},
 		}
-		defer resp.Body.Close()
+		checkValueRequest(values, url)
 
-		fmt.Println("response Status:", resp.Status)
-		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
-
-	} else {
+	case "article":
 		values := map[string]map[string]string{
 			"article": {
 				"title":     r.FormValue("title"),
@@ -142,20 +101,25 @@ func (a *App) HomePagePost(w http.ResponseWriter, r *http.Request) {
 				"image":     r.FormValue("image"),
 			},
 		}
-		jsonValue, _ := json.Marshal(values)
-		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
-		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
-		req.Header.Set("Content-Type", "application/json")
+		checkValueRequest(values, url)
 
-		client := &http.Client{}
-		resp, err := client.Do(req)
-		if err != nil {
-			panic(err)
-		}
-		defer resp.Body.Close()
-
-		fmt.Println("response Status:", resp.Status)
-		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
-
+	default:
+		json.NewEncoder(w).Encode("no created !!!")
 	}
+}
+
+func checkValueRequest(values map[string]map[string]string, url string) {
+
+	jsonValue, _ := json.Marshal(values)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
 }
