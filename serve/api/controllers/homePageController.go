5cd5e041c9ffb9a8208583fe8d08b235a377ed14
no1-bigcrab <nhiepphongts@gmail.com>
2020-05-29 14:44:29 +0700

    post data form shopify

--- serve/api/controllers/homePageController.go
+++ serve/api/controllers/homePageController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"golang-test/serve/api/models"
@@ -58,53 +59,103 @@ func (a *App) HomePageGet(w http.ResponseWriter, r *http.Request) {
 func (a *App) HomePagePost(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 	url := "https://" + r.FormValue("apikey") + ":" + r.FormValue("password") + "@" + r.FormValue("hostname") + "/admin/api/2020-04/" + r.FormValue("api-title") + ".json"
-	fmt.Println(url)
-	for key, values := range r.PostForm {
-		fmt.Println(key, values)
-	}
 
-	//fmt.Println(r.Form)
-	// req, err := http.NewRequest("POST", url, nil)
-	// req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
-	// req.Header.Set("Content-Type", "application/json")
-	// client := &http.Client{}
-	// resp, err := client.Do(req)
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// defer resp.Body.Close()
-	// fmt.Println("response Status:", resp.Status)
-	// body, _ := ioutil.ReadAll(resp.Body)
-	// var m interface{}
-	// json.Unmarshal(body, &m)
-	// fmt.Println(m)
-	// switch v := m.(type) {
-	// case map[string]interface{}:
-	// 	{
-	// 		switch h := v["products"].(type) {
-	// 		case []interface{}:
-	// 			{
-	// 				fmt.Println("ok")
-	// 				for _, x := range h {
-	// 					if x.(map[string]interface{})["role"] == "main" {
-	// 						json.NewEncoder(w).Encode(map[string]interface{}{"data": x.(map[string]interface{})["id"]})
-	// 					}
-	// 				}
-	// 			}
-	// 		default:
-	// 			fmt.Println("No type found")
-	// 		}
-	// 	}
-	// default:
-	// 	fmt.Println("No type found")
-	// }
-}
+	if r.FormValue("api-title") == "products" {
 
-func push(w http.ResponseWriter, resource string) {
-	pusher, ok := w.(http.Pusher)
-	if ok {
-		if err := pusher.Push(resource, nil); err == nil {
-			return
+		values := map[string]map[string]string{
+			"product": {
+				"title":       r.FormValue("title"),
+				"body_html":   r.FormValue("html-body"),
+				"vendor":      r.FormValue("vendor"),
+				"productType": r.FormValue("product-type"),
+			},
 		}
+		jsonValue, _ := json.Marshal(values)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
+
+	} else if r.FormValue("api-title") == "blogs" {
+
+		values := map[string]map[string]string{
+			"blog": {
+				"title":      r.FormValue("title"),
+				"metafields": "global",
+			},
+		}
+		jsonValue, _ := json.Marshal(values)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
+
+	} else if r.FormValue("api-title") == "pages" {
+
+		values := map[string]map[string]string{
+			"page": {
+				"title":     r.FormValue("title"),
+				"body_html": r.FormValue("html-body"),
+				"author":    r.FormValue("author"),
+			},
+		}
+		jsonValue, _ := json.Marshal(values)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
+
+	} else {
+		values := map[string]map[string]string{
+			"article": {
+				"title":     r.FormValue("title"),
+				"body_html": r.FormValue("html-body"),
+				"author":    r.FormValue("author"),
+				"tags":      r.FormValue("tags"),
+				"image":     r.FormValue("image"),
+			},
+		}
+		jsonValue, _ := json.Marshal(values)
+		req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonValue))
+		req.Header.Set("X-Shopify-Access-Token", r.FormValue("password"))
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		fmt.Println("response Status:", resp.Status)
+		json.NewEncoder(w).Encode(map[string]string{"code": resp.Status})
+
 	}
 }
--- serve/api/models/articleModel.go
+++ serve/api/models/articleModel.go
@@ -2,20 +2,11 @@ package models
 
 //Article cd
 type Article struct {
-	Title             string      `json:"title"`
-	BodyHTML          string      `json:"body_html"`
-	BlogID            int         `json:"blog_id"`
-	Author            string      `json:"author"`
-	UserID            interface{} `json:"user_id"`
-	SummaryHTML       interface{} `json:"summary_html"`
-	TemplateSuffix    interface{} `json:"template_suffix"`
-	Handle            string      `json:"handle"`
-	Tags              string      `json:"tags"`
-	AdminGraphqlAPIID string      `json:"admin_graphql_api_id"`
-	Image             struct {
-		Alt    string `json:"alt"`
-		Width  int    `json:"width"`
-		Height int    `json:"height"`
-		Src    string `json:"src"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Tags     string `json:"tags"`
+	BodyHTML string `json:"body_html"`
+	Image    struct {
+		Attachment string `json:"attachment"`
 	} `json:"image"`
 }
